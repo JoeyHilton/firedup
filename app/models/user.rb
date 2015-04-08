7c1410ef44f3c7915433f84c3d7eba031ffb9f56
JoeyHilton <shnek81@gmail.com>
2015-04-08 17:08:00 -0600

    Associations between user and education defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :profiles
+  has_many :educations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
