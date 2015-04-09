3ef1a97d2660db2b62b93f369dc7d8eb034992c4
Ryan Carter <rcarter@kokosurance.com>
2015-04-09 09:10:57 -0600

    pullmaster

--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,20 @@
 Rails.application.routes.draw do
-
-  resources :jobs
-  # get 'jobs/index'
-
-  # get 'jobs/show'
-
-  # get 'jobs/profile'
-
-  # get 'jobs/new'
-
-  # get 'jobs/create'
-
-  # get 'jobs/edit'
-
-  # get 'jobs/update'
-
-  # get 'jobs/destroy'
-
-  resources :certs
-
-
-  get 'profiles/new'
-
-  get 'profiles/create'
-
-  get 'profiles/update'
-
-  get 'profiles/edit'
-
-  get 'profiles/show'
-
-  get 'profiles/destroy'
-
-  get 'profiles/index'
-
-  devise_for :users
   
   root 'static#home'
 
   get 'static/about'
   get 'static/tour'
   get 'static/contact'
+  
+  resources :jobs
+
+  resources :certs
+
+  resources :profiles
+
+  devise_for :users
+  
+
 
 
   
