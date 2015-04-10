333b37c22fc276a16ce2c9d8c7f5416f28dee067
JoeyHilton <shnek81@gmail.com>
2015-04-09 20:59:49 -0600

    Attempt at seeding users and profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "test@test.com", password: "password", password_confirmation: "password")
+
+
+9.times do 
+  User.create(email: Faker::Internet.email,
+              name: Faker::Name.name,
+              password: "password",
+              password_confirmation: "password")
+end
+
+
+
+
+10.times do
+  Profile.create(up_fname: Faker::Name.first_name,
+                 up_lname: Faker::Name.last_name,
+                 up_address: Faker::Address.street_address,
+                 up_city: Faker::Address.city,
+                 up_state: Faker::Address.state,
+                 up_zip: Faker::Address.zip,
+                 up_twitter: Faker::Name.first_name,
+                 up_birthdate: Faker::Date.backward(2000),
+                 up_phone: Faker::PhoneNumber.phone_number,
+                 up_mobilephone: Faker::PhoneNumber.phone_number,
+                 up_gender: "male"
+                 up_secondemail: Faker::Internet.email,
+                 up_bio: Faker::Lorem.sentence
+                 user_id: User.ids.sample)
+end
