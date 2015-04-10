f9349444d819f0dfc8a82d742e69cbd8e96a9c5a
JoeyHilton <shnek81@gmail.com>
2015-04-10 09:14:14 -0600

    Changed link paths on nav_bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
 
   root 'static#home'
 
+  devise_for :users
+  resources :users, :only => [:show, :index]
+
   get '/profile/:id', to: 'profiles#show', as: :profile
 
-
-  get 'static/about'
-  get 'static/tour'
-  get 'static/contact'
-  
   shallow do
     resources :profiles do
       resources :jobs
@@ -17,8 +15,10 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
-  resources :users, :only => [:show, :index]
+  get 'static/about'
+  get 'static/tour'
+  get 'static/contact'
+  
   
 
 
