1b696862e68b6ac81be10ad0b7bc27b99ab6b87c
jobenscott <your.jobenscott@yahoo.com>
2015-04-09 19:00:19 -0600

    changes to root

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,13 +12,14 @@ class JobsController < ApplicationController
 
   def new
     @job = Job.new
+    @profile = Profile.find(params(:id))
   end
 
   def create
     @job = Job.new(job_params)
 
     if @job.save
-      redirect_to jobs_path
+      redirect_to profile_jobs_path
     else
       render :new
     end
@@ -32,7 +33,7 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
 
     if @job.update(job_params)
-      redirect_to jobs_path
+      redirect_to profile_jobs_path
     else
       render :edit
     end
@@ -41,7 +42,7 @@ class JobsController < ApplicationController
   def destroy
     @job = Job.find(params[:id])
     @job.destroy
-    redirect_to jobs_path
+    redirect_to profile_jobs_path
   end
 
   private
