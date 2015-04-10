b11afe5057fbb641a5d45de5ab7fcf226eb19025
Ryan Carter <cartervisuals@gmail.com>
2015-04-09 18:59:02 -0600

    did id migrations

---
+++ db/migrate/20150410005252_add_profile_id_to_certs.rb
@@ -0,0 +1,6 @@
+class AddProfileIdToCerts < ActiveRecord::Migration
+  def change
+    add_reference :certs, :profile, index: true
+    add_foreign_key :certs, :profiles
+  end
+end
---
+++ db/migrate/20150410005536_add_profile_id_to_educations.rb
@@ -0,0 +1,6 @@
+class AddProfileIdToEducations < ActiveRecord::Migration
+  def change
+    add_reference :educations, :profile, index: true
+    add_foreign_key :educations, :profiles
+  end
+end
