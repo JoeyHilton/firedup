d88ba6b1c645c6cacf39e8449fbc87c98281a15e
JoeyHilton <shnek81@gmail.com>
2015-04-13 21:42:16 -0600

    After sign-in and update redirects

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
 
 
-  # def after_sign_in_path_for(resource)
-  #   new_profile_path
-  # end
+  def after_sign_in_path_for(resource)
+    profile_path
+  end
+
+  def after_update_path_for(resource)
+    profile_path
+  end
+
   private
     def sign_up_params
       params.require(:user).permit(:email, :password, :password_confirmation)
