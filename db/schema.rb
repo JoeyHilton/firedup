aa13c438e43a51692502af1f43a130681c21be4a
Ryan Carter <cartervisuals@gmail.com>
2015-04-18 10:44:21 -0600

    user index

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150417194624) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +23,11 @@ ActiveRecord::Schema.define(version: 20150417194624) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "profile_id"
     t.integer  "user_id"
   end
 
+  add_index "certs", ["profile_id"], name: "index_certs_on_profile_id", using: :btree
   add_index "certs", ["user_id"], name: "index_certs_on_user_id", using: :btree
 
   create_table "educations", force: :cascade do |t|
@@ -39,9 +39,11 @@ ActiveRecord::Schema.define(version: 20150417194624) do
     t.string   "degree"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "profile_id"
     t.integer  "user_id"
   end
 
+  add_index "educations", ["profile_id"], name: "index_educations_on_profile_id", using: :btree
   add_index "educations", ["user_id"], name: "index_educations_on_user_id", using: :btree
 
   create_table "follows", force: :cascade do |t|
@@ -63,10 +65,12 @@ ActiveRecord::Schema.define(version: 20150417194624) do
     t.text     "description"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
+    t.integer  "profile_id"
     t.integer  "user_id"
     t.boolean  "current_employer", default: false
   end
 
+  add_index "jobs", ["profile_id"], name: "index_jobs_on_profile_id", using: :btree
   add_index "jobs", ["user_id"], name: "index_jobs_on_user_id", using: :btree
 
   create_table "likes", force: :cascade do |t|
@@ -101,12 +105,6 @@ ActiveRecord::Schema.define(version: 20150417194624) do
     t.integer  "sender_id"
   end
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "content"
@@ -134,16 +132,17 @@ ActiveRecord::Schema.define(version: 20150417194624) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "user_id"
+  end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -169,14 +168,20 @@ ActiveRecord::Schema.define(version: 20150417194624) do
     t.datetime "image_updated_at"
     t.string   "provider"
     t.string   "uid"
-    t.boolean  "current_employer",       default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "certs", "profiles"
+  add_foreign_key "certs", "profiles"
   add_foreign_key "certs", "users"
+  add_foreign_key "educations", "profiles"
+  add_foreign_key "educations", "profiles"
   add_foreign_key "educations", "users"
+  add_foreign_key "jobs", "profiles"
   add_foreign_key "jobs", "users"
   add_foreign_key "posts", "users"
+  add_foreign_key "profiles", "users"
+  add_foreign_key "profiles", "users"
 end
