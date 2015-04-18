3490d510278172c3e52fcfa8550285f3ff9f3d9a
jobenscott <your.jobenscott@yahoo.com>
2015-04-18 10:32:01 -0600

    added viewed column to messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417194624) do
+ActiveRecord::Schema.define(version: 20150418162507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,11 +92,12 @@ ActiveRecord::Schema.define(version: 20150417194624) do
   create_table "messages", force: :cascade do |t|
     t.string   "subject"
     t.text     "content"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "user_id"
     t.integer  "receiver_id"
     t.integer  "sender_id"
+    t.boolean  "viewed",      default: false
   end
 
   create_table "posts", force: :cascade do |t|
