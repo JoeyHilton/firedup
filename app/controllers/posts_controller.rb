a3dc0e33fc306dd3a8fe7b3e93f0359a3f02a477
JoeyHilton <shnek81@gmail.com>
2015-04-21 16:31:23 -0600

    Added paperclip to posts. Users can post images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,7 +48,7 @@ class PostsController < ApplicationController
   private
   
     def post_params
-        params.require(:post).permit(:content, :share_with, :user_id)
+        params.require(:post).permit(:content, :share_with, :user_id, :image)
     end
 
     def set_post
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,13 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200", xlarge: "300x300" }
+
   validates :content, presence: true
   validates :share_with, presence: true
+
+  validates_attachment :image, :size => { :in => 0..1000.kilobytes }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   SHARES = %w(Everyone Connections)
 
