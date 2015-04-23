d8f92b108b896eed8dd5690bc1674945a3fafc74
JoeyHilton <shnek81@gmail.com>
2015-04-23 13:17:03 -0600

    Added some validations to certs

--- app/models/cert.rb
+++ app/models/cert.rb
@@ -2,8 +2,8 @@ class Cert < ActiveRecord::Base
 
   belongs_to :user
 
-  # validates :title, presence: true
-  # validates :start_date, presence: true
-  # validates :expire_date, presence: true
+  validates :title, presence: true
+  validates :start_date, presence: true
+  validates :expire_date, presence: true
 
 end
