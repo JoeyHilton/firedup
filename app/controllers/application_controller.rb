d008a06148f9515f0e14d38fbae36240979bae23
jobenscott <your.jobenscott@yahoo.com>
2015-04-22 20:05:28 -0600

    loud exciting noisesgit add . I GOT IT TO SHOW THE NOTIFICATION ON THE NAVBAR WHEN THERE ARE PENDING CONNECTIONS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :navbar_pending_connection
 
+
   private
 
     def navbar_pending_connection
