d1399c8dd768cf19cc00fbce3503e0253790484f
JoeyHilton <shnek81@gmail.com>
2015-04-23 14:33:37 -0600

    Added validations to cert attachments

--- app/models/cert.rb
+++ app/models/cert.rb
@@ -6,4 +6,10 @@ class Cert < ActiveRecord::Base
   validates :start_date, presence: true
   validates :expire_date, presence: true
 
+  has_attached_file :attachment
+  # has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200", xlarge: "300x300"}
+  # validates_attachment :attachment, :content_type => {:content_type => %w(image/jpeg image/jpg image/png 
+  #                                                                     application/pdf application/msword 
+  #                                                                     application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
+  validates_attachment :attachment, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 end
