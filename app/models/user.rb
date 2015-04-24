a1ad79182fab42f7cc58cc00898af21d1d5bf167
JoeyHilton <shnek81@gmail.com>
2015-04-23 21:24:23 -0600

    Added validations to  user model for phone and zip

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,10 +25,10 @@ class User < ActiveRecord::Base
 
   # --------  above this line for devise only ----------------   
 
-  validates :birthdate, presence: true   
-  validates_format_of :zip, :with => /\A\d{5}-\d{4}|\A\d{5}\z/, :message => "should be in the form 12345 or 12345-1234"
-  validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
-  validates :mobile_phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+  # validates :birthdate   
+  validates_format_of :zip, :with => /\A\d{5}-\d{4}|\A\d{5}\z/, :message => "should be in the form 12345 or 12345-1234", :allow_blank => true
+  validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }, :allow_blank => true
+  validates :mobile_phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }, :allow_blank => true
 
   # has_one :profile, dependent: :destroy
   has_many :educations
