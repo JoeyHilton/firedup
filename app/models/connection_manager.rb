efaca5a37538ae614aeee4461080a299680a375a
Ryan Carter <cartervisuals@gmail.com>
2015-04-27 11:01:29 -0600

    connections manager comment out

--- app/models/connection_manager.rb
+++ app/models/connection_manager.rb
@@ -1,21 +1,24 @@
 class ConnectionManager
 
-@user_list = []
-@current_user_list = []
+#   @user_list = []
+#   @current_user_list = []
 
-@users = User.all
+#   @users = User.all
 
-@users.each do |user|
-  if user.followed_by?(current_user)
-    user_list << user
-  end
+#   @users.each do |user|
+#     if user.followed_by?(current_user)
+#       user_list << user
+#     end
 
-@users.each do |user|
-  if current_user.followed_by(user)
-    current_user_list << user
-  end
+#   @users.each do |user|
+#     if current_user.followed_by(user)
+#       current_user_list << user
+#     end
 
-@users.each do |user|
-  if current_user_list.include(user) && user_list.include(user)
-  end
+#   @users.each do |user|
+#     if current_user_list.include(user) && user_list.include(user)
+#     end
+#   end
+# end
+
 end
