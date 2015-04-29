76cfad833163f8fff0390126801f5beedb8f5193
JoeyHilton <shnek81@gmail.com>
2015-04-29 14:46:40 -0600

    Created contacts controller for emails

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,17 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash[:notice] = 'Thank you for your message. We will contact you soon!'
+      redirect_to new_contact_path
+    else
+      flash.now[:error] = 'Cannot send message.'
+      render :new
+    end
+  end
+end
