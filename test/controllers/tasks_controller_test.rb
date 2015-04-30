f4d46db6f488a17a0d39897b6c34ca8332d0ddb0
jobenscott <your.jobenscott@yahoo.com>
2015-04-30 14:34:26 -0600

    completed tasks controller test and ce models test

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,39 +1,52 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
+
+  def setup
+    @user = users(:one)
+    @other_user = users(:two)
+    @third_user = users(:three)
+    @task = tasks(:one)
+    @other_task = tasks(:two)
+    @new_params = { title: "Updated"}
+    sign_in @user
+  end
+
   test "should get index" do
-    get :index
+    get :index, user_id: @user.id
     assert_response :success
   end
 
   test "should get show" do
-    get :show
+    get :show, id: @task.id
     assert_response :success
   end
 
   test "should get new" do
-    get :new
+    get :new, user_id: @user.id
     assert_response :success
   end
 
   test "should get create" do
-    get :create
-    assert_response :success
+    assert_difference('Task.count', 1) do
+    post :create, user_id: @user.id, task: {title: "this" , due_date: Date.today, due_time: Date.today, category: "mycat", private: false, notes: "mytext" }
+    end
+    assert_redirected_to feed_path
   end
 
   test "should get edit" do
-    get :edit
+    get :edit, id: @task.id
     assert_response :success
   end
 
   test "should get update" do
-    get :update
-    assert_response :success
+    patch :update, id: @task.id, task: @new_params
+    assert_redirected_to feed_path
   end
 
   test "should get destroy" do
-    get :destroy
-    assert_response :success
+    get :destroy, id: @task.id
+    assert_redirected_to feed_path
   end
 
 end
