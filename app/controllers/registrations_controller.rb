0d7abb6fba213e6a428f21cdb41562adc512b06d
JoeyHilton <shnek81@gmail.com>
2015-05-05 16:17:53 -0600

    Added first and last names to strong params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -19,7 +19,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
     def sign_up_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :image)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :image)
     end
     def account_update_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password,
