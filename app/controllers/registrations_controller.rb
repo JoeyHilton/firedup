a44031bd00b9f1b7be6bbd824dcaa5e3eaee81cd
JoeyHilton <shnek81@gmail.com>
2015-05-05 10:58:49 -0600

    Facebook login working in dev

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,25 +11,11 @@ class RegistrationsController < Devise::RegistrationsController
     user_path(@user)
   end
 
-  # def update
-  #   @user = User.find(current_user.id)
-  #   email_changed = @user.email != params[:user][:email]
-  #   is_facebook_account = !@user.provider.blank?
+  protected
 
-  #   successfully_updated = if !is_facebook_account
-  #     @user.update_with_password(params[:user])
-  #   else
-  #     @user.update_without_password(params[:user])
-  #   end
-
-  #   if successfully_updated
-  #     # Sign in the user bypassing validation in case his password changed
-  #     sign_in @user, :bypass => true
-  #     redirect_to feed_path
-  #   else
-  #     render "edit"
-  #   end
-  # end
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   private
     def sign_up_params
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,8 +5,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      redirect_to edit_user_registration_path
+      if @user.sign_in_count == 1
+        flash[:notice] = "You have successfully signed up! Please fill-in your profile information."
+        redirect_to edit_user_registration_path
+      else
+        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+        redirect_to feed_path
+      end  
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
