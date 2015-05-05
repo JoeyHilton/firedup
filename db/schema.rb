7a85f36fa6ac30db01eb3419135d377b73680dda
Ryan Carter <cartervisuals@gmail.com>
2015-05-04 23:05:08 -0600

    Added archive column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429211337) do
+ActiveRecord::Schema.define(version: 20150505022722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,19 @@ ActiveRecord::Schema.define(version: 20150429211337) do
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "continuing_eds", force: :cascade do |t|
+    t.string   "topic"
+    t.date     "date"
+    t.integer  "hours"
+    t.string   "ClassType"
+    t.text     "Notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "continuing_eds", ["user_id"], name: "index_continuing_eds_on_user_id", using: :btree
 
   create_table "educations", force: :cascade do |t|
     t.string   "school"
@@ -197,17 +210,6 @@ ActiveRecord::Schema.define(version: 20150429211337) do
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
-  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
-  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
-
   create_table "tasks", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
@@ -218,6 +220,7 @@ ActiveRecord::Schema.define(version: 20150429211337) do
     t.text     "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "archive"
   end
 
   add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
@@ -263,6 +266,7 @@ ActiveRecord::Schema.define(version: 20150429211337) do
   add_foreign_key "ces", "users"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "continuing_eds", "users"
   add_foreign_key "educations", "users"
   add_foreign_key "jboards", "users"
   add_foreign_key "jobs", "users"
