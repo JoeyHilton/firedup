0285b596a58d0f8d36cc33ad4f25e1d405245adb
JoeyHilton <shnek81@gmail.com>
2015-05-05 09:07:30 -0600

    Took our if statement looking for birthdate

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,11 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      if @user.birthdate
-        redirect_to feed_path
-      else
-        redirect_to edit_user_registration_path
-      end  
+      redirect_to edit_user_registration_path
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
